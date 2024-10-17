624f867741e61fe40f4a4ac1a201d3f71200cef1
AniketSakpal19102001 <160087718+AniketSakpal19102001@users.noreply.github.com>
2024-10-17 23:24:50 +0530

    Update verifyToken.js
    added promise to verify token

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,21 +1,50 @@
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
-const  verifyToken = (req, res, next)=>{
+
+const verifyToken = async (req, res, next) => {
     const token = req.cookies.token;
-    console.log(token);
-    console.log(req.cookies);
+
     if (!token) {
-        res.sendStatus(401)
-    }else{
-        jwt.verify(token, config.jwtSecret, (err, user)=>{
-            if (err) {
-                res.sendStatus(403);
-            }else{
-                req.user = user;
-                
-                next();
-            }
-        })
+        return res.status(401).json({ message: "No token provided" });
     }
-}
+
+    try {
+        const user = await new Promise((resolve, reject) => {
+            jwt.verify(token, config.jwtSecret, (err, user) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(user);
+            });
+        });
+
+        req.user = user;
+        next();
+    } catch (err) {
+        res.sendStatus(403);
+    }
+};
+
 export default verifyToken;
+
+
+// import jwt from "jsonwebtoken";
+// import config from "../config/config.js";
+// const  verifyToken = (req, res, next)=>{
+//     const token = req.cookies.token;
+    
+//     if (!token) {
+//         res.status(401)
+//     }else{
+//         jwt.verify(token, config.jwtSecret, (err, user)=>{
+//             if (err) {
+//                 res.sendStatus(403);
+//             }else{
+//                 req.user = user;
+                
+//                 next();
+//             }
+//         })
+//     }
+// }
+// export default verifyToken;
